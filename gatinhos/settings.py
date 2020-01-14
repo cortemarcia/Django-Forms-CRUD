20deb4cc72b30f0bb904b8101e8f235624263804
Marcia <marcia@hotmail.com>
2020-01-14 16:48:50 -0300

    Adionando Login

--- gatinhos/settings.py
+++ gatinhos/settings.py
@@ -119,6 +119,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/static/',
-LOGIN_REDIRECT_URL ='/',
+STATIC_URL = '/static/'
+LOGIN_REDIRECT_URL ='/'
 LOGOUT_REDIRECT_URL= '/'
